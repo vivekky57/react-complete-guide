e78623d132c28becb886685eff53867b422bee5f
vivekky <vivekky57@gmail.com>
2022-03-26 02:40:46 +0530

    Adding normal JavaScript logic to components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,10 +3,17 @@ function ExpenseItem(props) {
   //   const expenseDate = new Date(2022, 3, 25);
   //   const expenseTitle = "car Insurance";
   //   const expenseAmount = 294.66;
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
