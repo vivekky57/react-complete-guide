f19ad11bb545263b3d280892b0a6f0d1669583d8
vivekky <vivekky57@gmail.com>
2022-03-26 04:22:17 +0530

    'Composition' (children props)

--- src/App.js
+++ src/App.js
@@ -26,9 +26,8 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <div className="expenses">
-        <Expenses items={expenses} />
-      </div>
+
+      <Expenses items={expenses} />
     </div>
   );
 }
